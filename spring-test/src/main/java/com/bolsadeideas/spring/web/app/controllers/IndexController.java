7f21c73c5ede58657435b2936094fda74df9fab7
kevin.diaz <kevin.diaz@CLQL001241>
2020-02-21 12:52:22 -0600

    RequestMapping

--- spring-test/src/main/java/com/bolsadeideas/spring/web/app/controllers/IndexController.java
+++ spring-test/src/main/java/com/bolsadeideas/spring/web/app/controllers/IndexController.java
@@ -1,13 +1,19 @@
 package com.bolsadeideas.spring.web.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping({"/index", "/", "/home"})
+	@GetMapping({"/index", "/", "/home", ""})
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola spring framework!");
 		return "index";
